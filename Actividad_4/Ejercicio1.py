8807f61768309365db437fa1ce1c837c3ee6cd51
Andres18-pixel <ericksonlopez@uamv.edu.ni>
2025-04-21 11:32:19 -0600

    Ejercicio 1. Verificar si un # está dentro de un rango

---
+++ Actividad_4/Ejercicio1.py
@@ -0,0 +1,8 @@
+rango_inicial = int(input("Escribe ek inicio del rango: "))
+rango_final = int(input("Escribe el final del rango: "))
+numero = int(input("Dime un numero: "))
+if(numero >=rango_inicial and numero <= rango_final):
+    print("El número está dentro del rango.")
+else:
+    print("El número está fuera del rango.")
+    
